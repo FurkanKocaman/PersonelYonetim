788dd988d5a11623323f349cb30f249ae8cc4691
canerAcil <caneracil578@gmail.com>
2025-03-26 08:33:21 +0300

    KisiselBilgiler sekmesi
    Kişisel Bilgilerim sekmesinin çoğu tamamlandı

--- PersonelYonetim.UI/src/views/ProfilView.vue
+++ PersonelYonetim.UI/src/views/ProfilView.vue
@@ -79,7 +79,7 @@ const setActiveTab = (tab: string) => {
       </div>
 
 
-
+        <!-- profil kısmı -->
        <div v-if="activeTab === 'profilim'" class="space-y-6">
        
         <div class="bg-white dark:bg-neutral-800 rounded-lg shadow-sm p-6 w-[600px] h-[500px]" style="margin-left: 100px;">
@@ -273,14 +273,287 @@ const setActiveTab = (tab: string) => {
     </div>
 </div>
 
-
-
-
-
-
-     
-            
       </div> 
+
+      <!-- Kişisel Bilgilerim sekmesi -->
+
+      <div v-if="activeTab === 'kisiselBilgilerim'" class="space-y-6">
+
+          <!-- Vatandaşlık -->
+      <div class="bg-gray-100 p-4 rounded-lg shadow-md w-[750px]" style="margin-left: 60px;">
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">Vatandaşlık</h2>
+        <span class="text-blue-400 cursor-pointer">✏️</span>
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3" style="height: 310px;">
+        <!-- Doğum Tarihi - Cinsiyet -->
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Doğum Tarihi</p>
+                <p class="text-gray-900 text-sm">1 May 1989</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 300px;" >
+                <p class="text-gray-600 text-sm">Cinsiyet</p>
+                <p class="text-gray-900 text-sm">Erkek</p>
+            </div>
+        </div>
+
+        <!-- Engel Derecesi -->
+        <div>
+            <p class="text-gray-600 text-sm">Engel Derecesi</p>
+            <p class="text-gray-900">—</p>
+        </div>
+
+        <hr class="my-4 border-gray-300 dark:border-gray-600">
+<br>
+        <!-- Uyruğu - Kimlik Numarası -->
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Uyruğu</p>
+                <p class="text-gray-900 text-sm">Türkiye</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 339px;" >
+                <div >
+                    <p class="text-gray-600 text-sm" >Kimlik Numarası</p>
+                    <p class="text-gray-900 text-sm">41557015086</p>
+                </div>
+                <br>           
+            </div>
+        </div>
+
+        <!-- Askerlik Durumu -->
+        <div>
+            <p class="text-gray-600 text-sm">Askerlik Durumu</p>
+            <p class="text-gray-900 text-sm">Tamamlandı</p>
+        </div>
+    </div>
+</div>
+
+
+<br>
+    <!-- Eğitim -->
+<div class="bg-gray-100 p-4 rounded-lg shadow-md w-[750px]" style="margin-left: 60px;">
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">Eğitim</h2>
+     
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3" style="height: 150px;">
+       
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Eğitim Durumu</p>
+                <p class="text-gray-900 text-sm">Mezun</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 300px;" >
+                <p class="text-gray-600 text-sm">Tamamlanan En Yüksek Eğitim Seviyesi</p>
+                <p class="text-gray-900 text-sm">Yüksek Lisans</p>
+            </div>
+        </div>
+<br>
+        
+        <div>
+            <p class="text-gray-600 text-sm">Son Tamamlanan Eğitim Kurumu</p>
+            <p class="text-gray-900">—</p>
+        </div>
+
+    </div>
+</div>
+
+<br>
+    <!-- Aile -->
+<div class="bg-gray-100 p-4 rounded-lg shadow-md w-[750px]" style="margin-left: 60px;">
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">Aile</h2>
+        <span class="text-blue-400 cursor-pointer">✏️</span>
+     
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3" style="height: 150px;">
+       
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Medeni Hal</p>
+                <p class="text-gray-900 text-sm">Evli</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 300px;" >
+                <p class="text-gray-600 text-sm">Eş Çalışma Durumu</p>
+                <p class="text-gray-900 text-sm">—</p>
+            </div>
+        </div>
+
+        <br>
+        <div>
+            <p class="text-gray-600 text-sm">Çocuk Sayısı</p>
+            <p class="text-gray-900">—</p>
+        </div>
+
+    </div>
+   
+</div>
+<br>
+   <!-- Adres -->
+<div class="bg-gray-100 p-4 rounded-lg shadow-md w-[750px]" style="margin-left: 60px;">
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">Adres</h2>
+     
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3" style="height: 280px;">
+       
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Adres</p>
+                <p class="text-gray-900 text-sm">Aydınlıkevler mah. Hasan Paşa cad. kardeşler apt. sitesi no:74 iç kapı no:3 ortahisar</p>
+            </div>
+        </div>
+
+        
+        <div>
+            <p class="text-gray-600 text-sm">Adres (devam)</p>
+            <p class="text-gray-900">—</p>
+        </div>
+        <br>
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Şehir</p>
+                <p class="text-gray-900 text-sm">Trabzon</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 300px;" >
+                <p class="text-gray-600 text-sm">Ülke</p>
+                <p class="text-gray-900 text-sm">Türkiye</p>
+                
+            </div>
+            
+        </div>
+<br>
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Posta Kodu</p>
+                <p class="text-gray-900 text-sm">61000</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 279px;" >
+                <p class="text-gray-600 text-sm">Telefon</p>
+                <p class="text-gray-900 text-sm">—</p>
+                
+            </div>
+            
+        </div>
+       
+
+    </div>
+   
+</div>
+<br>
+<!-- Banka hesabı -->
+
+<div class="bg-gray-100 p-4 rounded-lg shadow-md w-[750px]" style="margin-left: 60px;">
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">Banka Hesabı</h2>
+        <span class="text-blue-400 cursor-pointer">✏️</span>
+     
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3" style="height: 150px;">
+       
+        
+
+      <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Banka Adı</p>
+                <p class="text-gray-900 text-sm">—</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 300px;" >
+                <p class="text-gray-600 text-sm">Hesap Tipi</p>
+                <p class="text-gray-900 text-sm">—</p>
+                
+            </div>
+            
+        </div>
+<br>
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Hesap Numarası</p>
+                <p class="text-gray-900 text-sm">—</p>
+            </div>
+            <div class="flex-grow" style="margin-left: 257px;" >
+                <p class="text-gray-600 text-sm">IBAN</p>
+                <p class="text-gray-900 text-sm">TR780001001225565740685001</p>
+                
+            </div>
+            
+        </div>
+       
+
+        
+
+    </div>
+   
+</div>
+<br>
+    <!-- İletişim -->
+<div class="bg-gray-100 p-4 rounded-lg shadow-md w-96 " style="margin-left: 850px; width:560px; margin-top:-1622px; " >
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">İletişim</h2>
+        <span class="text-gray-400 cursor-pointer">✏️</span>
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3">
+
+        
+        <div class="flex items-center space-x-3">
+            <span class="text-gray-400">👀</span>
+            <div class="flex-1">
+                <p class="text-gray-600 text-sm">E-Posta (Kişisel)</p>
+                <p class="text-gray-400">—</p>
+            </div>
+        </div>
+        <hr class="my-4 border-gray-300 dark:border-gray-600">
+       
+        <div class="flex items-center space-x-3">
+            <span class="text-gray-400">👀</span>
+            <div class="flex-1">
+                <p class="text-gray-600 text-sm">Telefon (Kişisel)</p>
+                <p class="text-blue-600 font-medium">+90 551 159 1957</p>
+            </div>
+            <span class="text-gray-400 cursor-pointer">📋</span>
+        </div>
+    </div>
+</div>
+
+<!-- Acil Durum -->
+
+<div class="bg-gray-100 p-4 rounded-lg shadow-md w-[560px]" style="margin-left: 850px;">
+    <div class="flex justify-between items-center mb-2">
+        <h2 class="text-lg font-semibold text-gray-700">Acil Durum</h2>
+        <span class="text-blue-400 cursor-pointer">✏️</span>
+     
+    </div>
+    <div class="bg-white p-4 rounded-lg space-y-3" style="height: 220px;">
+       
+        <div class="flex justify-between">
+            <div>
+                <p class="text-gray-600 text-sm">Acil durumda erişilebilecek kişinin bilgileri</p>
+               
+            </div>
+            
+        </div>
+        
+        <div>
+            <p class="text-gray-600 text-sm">Adı Soyadı</p>
+            <p class="text-gray-900">—</p>
+        </div>
+
+        <div>
+          <p class="text-gray-600 text-sm">Telefon</p>
+          <p class="text-gray-900">—</p>
+        </div>
+        <div>
+          <p class="text-gray-600 text-sm">Yakınlık Derecesi</p>
+          <p class="text-gray-900">—</p>
+        </div>
+
+    </div>
+</div>
+
+<br>
+
+</div>
 
       
 
